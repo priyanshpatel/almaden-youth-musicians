da8c1f16595547decb8950c27770788b01a3d0f5
Priyansh Patel <43277093+priyanshpatel@users.noreply.github.com>
2021-04-10 20:18:44 -0700

    config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // const api_url = "http://localhost:3001"
-const api_url = "https://almadenyouthmusicians-be.herokuapp.com"
+const api_url = "https://almadenyouthmusicans-be.herokuapp.com"
 module.exports = {
     api_url: api_url,
 }
